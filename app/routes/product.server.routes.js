50ed488e735ff60d0d62587fa481bda75f108d67
anton29 <abr8892@yahoo.com>
2015-06-23 18:54:05 -0400

    Delete product.server.routes.js

--- app/routes/product.server.routes.js
+++
@@ -1,6 +0,0 @@
-//working ??
-// may not be needed
-// module.exports = function(app){
-//   var product = require('../controllers/product.server.controller');
-//   app.get('/', product.render);
-// };
